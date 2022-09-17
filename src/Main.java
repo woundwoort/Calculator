ee7416dda03c05de4efce442f95117f2fed95279
woundwoort <woundwoort@yandex.ru>
2022-09-17 15:46:31 +0300

    // Изменено название класса на "main"

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 
-public class Calculator {
+public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in)); // создаем поток
